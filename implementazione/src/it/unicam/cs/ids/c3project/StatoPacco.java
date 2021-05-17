54a90896923353e4580dbbf6961edda75fd64dff
Eug-Car <eugenioc_@outlook.com>
2021-05-17 12:26:55 +0200

    Implementazione e modifiche classi di Progetto
    Implementazioni di: Articolo, Corriere, gestoreSpedizioni, Pacco, StatoPacco,TipologiaContenuto,TIpologiaCorriere

---
+++ implementazione/src/it/unicam/cs/ids/c3project/StatoPacco.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.ids.c3project;
+
+public enum StatoPacco {
+    NON_PARTITO,
+    IN_CONSEGNA,
+    CDEPOSITATO,
+    CONSEGNATO
+}
---
+++ implementazione/src/it/unicam/cs/ids/c3project/TipologiaContenuto.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.ids.c3project;
+
+public enum TipologiaContenuto {
+    FRAGILE,
+    CONTIENTE_BATTERIE_A_LITIO,
+    INFIAMMABILE,
+    PESANTE
+}
